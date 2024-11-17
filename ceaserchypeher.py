bdd17867dd20485afab151b5efff1de64afb1b18
Izadora <izadorasantosdesousa@gmail.com>
2024-11-17 15:03:44 -0300

    adding more mini-projects

---
+++ ceaserchypeher.py
@@ -0,0 +1,23 @@
+print("Let1s play a game ")
+print(r"""
+  ___ __ _  ___  ___  __ _ _ __ 
+ / __/ _` |/ _ \/ __|/ _` | '__|
+| (_| (_| |  __/\__ \ (_| | |   
+ \___\__,_|\___||___/\__,_|_|   
+
+      _       _               
+     (_)     | |              
+  ___ _ _ __ | |__   ___ _ __ 
+ / __| | '_ \| '_ \ / _ \ '__|
+| (__| | |_) | | | |  __/ |   
+ \___|_| .__/|_| |_|\___|_|   
+       | |                    
+       |_|      
+""")
+
+encodeordecode = input("Type 'encode' to encrypty and 'decode' to decrypt");
+message = input("Type your message ")
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
