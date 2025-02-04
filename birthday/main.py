296f084bee79761cae576132adb1381b6657a8e7
izadora <izadorasantosdesousa@gmail.com>
2025-02-04 12:02:11 -0300

    Adicona envio de email automatico pra pessoas que estao fazendo aniversario

---
+++ birthday/main.py
@@ -0,0 +1,39 @@
+import datetime as dt
+import pandas
+import random
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MY_EMAIL = os.getenv("EMAIL")
+MY_PASSWORD = os.getenv("PASSWORD")
+
+random_num = random.randint(1, 3)
+random_letter = f"letter_{random_num}.txt"
+
+now = dt.datetime.now()
+month_day = (now.month, now.day)
+
+data = pandas.read_csv("birth.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if month_day in birthdays_dict:
+    birthday_person = birthdays_dict[month_day]
+
+    with open(f"./letter_templates/{random_letter}", "r") as file:
+        letter_data = file.read()
+        personalized_letter = letter_data.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n{personalized_letter}"
+        )
+
+    print("E-mail enviado com sucesso!")
+else:
+    print("Hoje não é aniversário de ninguém.")
